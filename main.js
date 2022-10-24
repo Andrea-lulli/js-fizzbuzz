4b563709d9c0265d08311b88e53a7f7cf84c8d4b
Andrea-Lulli <andrea.lulli89@gmail.com>
2022-10-24 09:39:01 +0200

    esercizio completato

--- main.js
+++ main.js
@@ -3,26 +3,48 @@
 //i multipli di 5 stampi “Buzz”.
 // Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.
 
-let Numeri = " ";
+// let Numeri = document.getElementById("numeri"); 
+
 
 
 for (let i = 0; i < 100; i++) {
 
-  if ( i % 3 === 0 ){
-    
-    Numeri +=  "Fizz" ;
-    
-  } else if ( i % 5 === 0 ) {
-    Numeri += "Buzz";
-  } else if ( i % 3 === 0 && i % 5 === 0 ) {
-    Numeri += "FizBuzz";
-  } else {
-    Numeri += i;
-  } 
+  let divNuovo = document.createElement("div");
+  divNuovo.classList.add("box-js");
   
+  
+
+   if ( i % 3 === 0 ){
+    
+     divNuovo.innerHTML += "Fizz" ;
+     divNuovo.classList.add("red")
+     console.log(divNuovo); 
+    
+   } else if ( i % 5 === 0 ) {
+    divNuovo.innerHTML  += "Buzz";
+    divNuovo.classList.add("yellow")
+    console.log(divNuovo); 
+
+   } else if ( i % 3 === 0 && i % 5 === 0 ) {
+    divNuovo.innerHTML  += "FizBuzz";
+    console.log(divNuovo); 
+
+   } else {
+    divNuovo.innerHTML  += i;
+    console.log(divNuovo); 
+
+   } 
+
+   
+   document.getElementById("numeri").append(divNuovo);
+
+
 }
 
-let text = document.getElementById("numeri").innerHTML = `${Numeri}`;
+
+
+
+
 
 
 
